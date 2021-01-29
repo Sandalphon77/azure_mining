30a736f2c78a616d0e5e364e1817973b2b01fe83
Sandalphon77 <sandalphon.at@gmail.com>
2021-01-29 09:19:48 +0100

    Update 20210129

--- setup_vm.sh
+++ setup_vm.sh
@@ -3,19 +3,19 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/Sandalphon77/azure_mining/azure_script
+git clone https://github.com/Sandalphon77/azure_mining
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
 else
       echo "checkout tag $gittag"
-      cd azure-cloud-mining-script
+      cd azure_mining
       git checkout $gittag
       cd ..
 fi
 
-cd azure-cloud-mining-script
-azure_script/compile_and_config.sh
+cd azure_mining
+./compile_and_config.sh
 
 
 
